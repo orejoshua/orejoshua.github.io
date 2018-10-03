52cc255c632e14e3d6185e811399c2271843fba8
orejoshua <40673670+orejoshua@users.noreply.github.com>
2018-10-03 09:04:08 +0100

    Create index.js

---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+export const PATH_BASE = 'http://api.openweathermap.org/data/2.5';
+export const PATH_SEARCH = '/weather?q=';
+export const PATH_FORECAST = '/forecast/daily?q=';
+export const PARAM_TYPE = 'type=accurate';
+export const PARAM_UNIT = 'units=imperial';
+export const PARAM_APPID = 'APPID=11b05c53b456df07280247cdd575530f';
+export const PARAM_DAYS = '&cnt=5';
